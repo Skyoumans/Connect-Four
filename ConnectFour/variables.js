4c49bc89a0843da374ee94af0e8a77366b6526b8
Skyoumans <skyyoumans@gmail.com>
2017-09-13 15:58:19 -0400

    created varable.js page, adding config, board, and current player variables.

--- ConnectFour/variables.js
+++ ConnectFour/variables.js
@@ -1,9 +1,19 @@
-// Create variables for use in our game.
+// Create variables for use in the game.
 
 // Config object.
-
-// Define the empty board as a two-dimensional array, full of zeros. In our
+var config = {
+    blkPlayerName: "Player One",
+    redPlayerName: "Player Two",
+    claimedMsg: "This space has already been claimed. Make a different choice.",
+    drawMsg: "The game is a draw",
+    winMsg: "The winner is: ",
+    startingPlayer: "black",
+    numToWin: 4,
+}
+// Define the empty board as a two-dimensional array, full of zeros. In the
 // game, 0 represents empty, 'red' represents a red disc, and 'black' represents
 // a black disc.
+var board = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]];
 
 // Set the starting player.
+var currentPlayer = config.startingPlayer;
